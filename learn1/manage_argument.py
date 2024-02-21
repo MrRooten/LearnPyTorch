d6d66eb481c1e8a40d9e7412e9a2d6e1dc433af5
hello_world <junzun@sdf.com>
2024-02-21 17:07:50 +0800

    update

---
+++ learn1/manage_argument.py
@@ -0,0 +1,24 @@
+import torch
+from torch import nn
+
+net = nn.Sequential(nn.Linear(4, 8), nn.ReLU(), nn.Linear(8, 1))
+X = torch.rand(size=(2, 4))
+print(net[2].state_dict())
+
+print(*[(name, param.shape) for name, param in net[0].named_parameters()])
+print(*[(name, param.shape) for name, param in net.named_parameters()])
+
+def block1():
+    return nn.Sequential(nn.Linear(4, 8), nn.ReLU(),
+                         nn.Linear(8, 4), nn.ReLU())
+
+def block2():
+    net = nn.Sequential()
+    for i in range(4):
+        # 在这里嵌套
+        net.add_module(f'block {i}', block1())
+    return net
+
+rgnet = nn.Sequential(block2(), nn.Linear(4, 1))
+print(rgnet(X))
+print(rgnet)
